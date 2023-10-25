0390940db0106dff5880191b23f3c4e6b69aabb2
OH <rudals2148@naver.com>
2023-10-25 20:06:45 +0900

    ui 변경

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -4,21 +4,15 @@ import "@aws-amplify/ui-react/styles.css";
 import awsmobile from "../aws-exports";
 import Navbar from "../components/layout/Navbar";
 import Board from "../components/Board";
-import AdminBoard from "./AdminBoard";
 import "../styles/main.scss";
 
 Amplify.configure(awsmobile)
 
 const AuthComponent = ({ user, signOut }: WithAuthenticatorProps) => {
-	console.log(user)
   return (
 		<main>
 			<Navbar user={user} signOut={signOut} />
-			{user?.username === "관리자계정" ?
-				<AdminBoard /> :
-				<Board /> 
-			}
-			
+			<Board /> 
 		</main>
   )
 }
