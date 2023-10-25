c8199a419859d7cf0d81b3586871a48b5d49385e
OH <rudals2148@naver.com>
2023-10-25 20:07:00 +0900

    로딩화면 추가 및 타입 분리

--- src/components/BoardDetail.tsx
+++ src/components/BoardDetail.tsx
@@ -3,20 +3,14 @@ import { useEffect, useState } from "react";
 import { API, Auth } from "aws-amplify";
 import Swal from "sweetalert2";
 import Button from "./ui/Button";
-
-interface ItemDataProps {
-	id: string
-	title: string
-	content: string
-	user: string
-	createdAt: string
-	viewCount: number
-}
+import { ItemDataProps } from "../@type/type";
+import Loading from "./ui/Loading";
 
 const BoardDetail = () => {
 	const [userName, setUserName] = useState<string | null>(null);
   const [data, setData] = useState<ItemDataProps>();
 	const [editMode, setEditMode] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const [editTitle, setEditTitle] = useState("");
 	const [editContent, setEditContent] = useState("");
 	const { id } = useParams();
@@ -69,66 +63,66 @@ const BoardDetail = () => {
   }
 
 	useEffect(() => {
+		setLoading(true);
 		Auth.currentAuthenticatedUser().then(user => setUserName(user.username))
     const fetchData = async () => {
       const result = await API.get("boardapi", `/board/${id}`, {});
 			setData(result.data.Item);
+			setLoading(false);
     };
-		console.log(data)
     fetchData();
   }, []);
-	
-	if (!data) {
-    return <div>Loading...</div>;
-  }
 
 	return (
-		<div className="detail__container">
-      <section className="detail__header">
-				{editMode ? (
-					<input className="edit__title" value={editTitle} onChange={e => setEditTitle(e.target.value)} />
-				) : (
-					<div className="detail__title">{data.title}</div>
-				)}
-				<div className="detail__info">
-					<div className="detail__left">
-						<span>{data.createdAt}</span>
-						<span>{data.user}</span>
+		<>
+			{loading && <Loading />}
+			<div className="detail__container">
+				<section className="detail__header">
+					{editMode ? (
+						<input className="edit__title" value={editTitle} onChange={e => setEditTitle(e.target.value)} />
+					) : (
+						<div className="detail__title">{data?.title}</div>
+					)}
+					<div className="detail__info">
+						<div className="detail__left">
+							<span>{data?.createdAt.split("T")[0]}</span>
+							<span>{data?.user}</span>
+						</div>
+						<div className="detail__right">
+							<span className="detail__view">
+								<span>👁‍🗨</span>
+								<span>{data?.viewCount}</span>
+							</span>
+						</div>
 					</div>
-					<div className="detail__right">
-						<span className="detail__view">
-							<span>👁‍🗨</span>
-							<span>{data.viewCount}</span>
-						</span>
-					</div>
+				</section>
+				<section className="detail__body">
+					{editMode ? (
+						<textarea className="edit__content" value={editContent} onChange={e => setEditContent(e.target.value)} />
+					) : (
+						<p>{data?.content}</p>
+					)}
+				</section>
+				<div className="detail__bottom">
+					<Button text="목록으로" onClick={() => navigate("/")}></Button>
+					{(userName === data?.user || userName === "관리자계정") && (
+						<div className="detail__button">
+							{editMode ? (
+								<>
+									<Button text="저장" onClick={onUpdateBoard}></Button>
+									<Button text="취소" onClick={() => setEditMode(false)}></Button>
+								</>
+							) : (
+								<>
+									<Button text="수정" onClick={() => setEditMode(true)}></Button>
+									<Button text="삭제" onClick={onDeleteBoard}></Button>
+								</>
+							)}
+						</div>
+					)}
 				</div>
-			</section>
-			<section className="detail__body">
-        {editMode ? (
-          <textarea className="edit__content" value={editContent} onChange={e => setEditContent(e.target.value)} />
-        ) : (
-          <p>{data.content}</p>
-        )}
-      </section>
-			<div className="detail__bottom">
-				<button onClick={() => navigate("/")}>목록으로</button>
-				{userName === data.user && (
-          <div className="detail__button">
-            {editMode ? (
-              <>
-                <Button text="저장" onClick={onUpdateBoard}></Button>
-                <Button text="취소" onClick={() => setEditMode(false)}></Button>
-              </>
-            ) : (
-              <>
-                <Button text="수정" onClick={() => setEditMode(true)}></Button>
-                <Button text="삭제" onClick={onDeleteBoard}></Button>
-              </>
-            )}
-          </div>
-        )}
 			</div>
-    </div>
+		</>
 	)
 }
 
