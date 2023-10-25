0ceae8d96a44c31936862f15f9648e0694875e4d
OH <rudals2148@naver.com>
2023-10-25 20:05:01 +0900

    어드민 계정을 위한 함수 수정

--- amplify/backend/function/boardfunction/src/app.js
+++ amplify/backend/function/boardfunction/src/app.js
@@ -1,14 +1,3 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-
 const AWS = require("aws-sdk");
 const { v4: uuid } = require("uuid");
 const docClient = new AWS.DynamoDB.DocumentClient();
@@ -77,6 +66,7 @@ app.post("/board", function(req, res) {
       content: req.body.content,
       createdAt: req.body.createdAt,
       viewCount: req.body.viewCount,
+      isAdmin: req.body.isAdmin
     }
   }
   docClient.put(params, function(err, data) {
@@ -127,7 +117,4 @@ app.listen(3000, function() {
     console.log("App started")
 });
 
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
 module.exports = app
