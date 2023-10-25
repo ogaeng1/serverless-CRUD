2f27ef04904f803395f7f97c446ac0d1f8865ca8
OH <rudals2148@naver.com>
2023-10-25 20:08:04 +0900

    ui 변경

--- src/components/WriteForm.tsx
+++ src/components/WriteForm.tsx
@@ -2,6 +2,7 @@ import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import { useState } from "react";
 import { API } from "aws-amplify";
 import Button from "./ui/Button";
+import Modal from "./ui/Modal";
 
 type modalProps = {
 	formModal: boolean
@@ -13,11 +14,12 @@ type WriteFormProps = WithAuthenticatorProps & modalProps
 const WriteForm = ({ user, formModal, setFormModal }: WriteFormProps) => {
 	const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const currentDate = new Date().toISOString().split("T")[0];
+  const currentDate = new Date().toISOString();
+	const isAdmin = user?.username === "관리자계정";
 	
 	const onAddBoard = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+		
     const data = {
       body: {
 				user: user?.username,
@@ -25,7 +27,7 @@ const WriteForm = ({ user, formModal, setFormModal }: WriteFormProps) => {
         content: content,
         createdAt: currentDate,
         viewCount: 0,
-        like: 0
+				isAdmin: isAdmin
       }
     };
   
@@ -33,22 +35,24 @@ const WriteForm = ({ user, formModal, setFormModal }: WriteFormProps) => {
 		setFormModal(!formModal)
   }
 	return (
-		<form onSubmit={onAddBoard} className="board__form">
-			<section className="board__title">
-				<label>제목</label>
-				<input type="text" placeholder="제목을 입력하세요" value={title} 
-					required maxLength={30} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-				/>
-			</section>
-			<section className="board__content">
-				<label>내용</label>
-				<textarea value={content} placeholder="내용을 입력하세요" onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)} />
-			</section>
-			<section className="board__button__box">
-				<Button className="cancel__button" text="취소" onClick={() => setFormModal(!formModal)}></Button>
-				<Button type="submit" className="submit__button" text="등록"></Button>
-			</section>
-		</form>
+		<Modal>
+			<form onSubmit={onAddBoard} className="board__form">
+				<section className="board__title">
+					<label>제목</label>
+					<input type="text" placeholder="제목을 입력하세요" value={title} 
+						required maxLength={30} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+					/>
+				</section>
+				<section className="board__content">
+					<label>내용</label>
+					<textarea value={content} placeholder="내용을 입력하세요" onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)} />
+				</section>
+				<section className="board__button__box">
+					<Button className="cancel__button" text="취소" onClick={() => setFormModal(!formModal)}></Button>
+					<Button type="submit" className="submit__button" text="등록"></Button>
+				</section>
+			</form>
+		</Modal>
 
 	)
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,20 +1,25 @@
 import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import { useState } from "react";
 import WriteForm from "../WriteForm";
+import Button from "../ui/Button";
+import UserList from "../UserList";
 
 const Navbar = ({ user, signOut }: WithAuthenticatorProps) => {
+	const [userListModal, setUserListModal] = useState(false);
 	const [formModal, setFormModal] = useState(false);
 
 	return (
 		<>
 			<div className="navbar">
-				<button onClick={() => setFormModal(!formModal)}>글쓰기</button>
+				{user?.username === "관리자계정" &&
+					<Button text="유저목록" onClick={() => setUserListModal(!userListModal)}></Button>
+				}
+				<Button text={user?.username === "관리자계정" ? "공지쓰기" : "글쓰기"} onClick={() => setFormModal(!formModal)}></Button>
 				<span className="navbar__user">{user?.username}</span>님
-				<button className="navbar__logout" onClick={signOut}>로그아웃</button>
+				<Button text="로그아웃" className="navbar__logout" onClick={signOut}></Button>
 			</div>
-			<div className={`${formModal ? "modal-background" : ""}`}>
-				{formModal && <WriteForm user={user} formModal={formModal} setFormModal={setFormModal} />}
-			</div>
+			{formModal && <WriteForm user={user} formModal={formModal} setFormModal={setFormModal} />}
+			{userListModal && <UserList userListModal={userListModal} setUserListModal={setUserListModal} />}
 		</>
 	)
 }
